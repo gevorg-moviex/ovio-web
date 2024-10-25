4da5584be85204398cc2939a4d5ddef8ae8c9d5c
gevorg-moviex <gevorg.sahakyan.ait@gmail.com>
2024-10-25 23:29:37 +0400

    Adding suggestion part in website and some changes

--- src/Components/Cards/cards.jsx
+++ src/Components/Cards/cards.jsx
@@ -1,59 +1,57 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import 'swiper/swiper-bundle.css';
 import { cardsAm } from "../../../dataAm";
-import "./cards.css"
 import { Navigation } from "swiper/modules";
 
 export default function Cards() {
     return (
-        <div style={{padding: "0 130px"}}>
+        <div className="px-3 product340x:px-7 product660x:px-28">
             <div>
-                <h1>{cardsAm.title}</h1>
+                <h1 className="text-3xl font-bold">{cardsAm.title}</h1>
             </div>
             <Swiper
                 slidesPerView="auto"
-                spaceBetween={50}
+                spaceBetween={40}
                 pagination={{clickable: false}}
-                navigation
                 modules={[Navigation]}
-                style={{cursor: "grab"}}
+                style={{cursor: "grab", padding: "40px 0", borderRadius: "0 21px"}}
             >
                 {cardsAm.products.map((item) => (
-                    <SwiperSlide key={item.number}>
-                        <div 
-                            style={
-                                {
-                                    display: "flex",
-                                    gap: "20px",
-                                    height: "152px",
-                                    boxShadow: "0 8px 30px 0 rgba(0,0,0,.15)",
-                                    borderRadius: "12px",
-                                }
-                            }
-                        >
-                            <div
-                                style=
-                                {
-                                    { 
-                                        backgroundColor: item.bgColor,
+                    <SwiperSlide key={item.number} style={{width: "453px", borderRadius: "20px"}}>
+                            <div 
+                                style={
+                                    {
                                         display: "flex",
-                                        justifyContent: "center",
-                                        alignItems: "center",
-                                        borderRadius: "21px 120px 120px 21px",
-                                        padding: "0 40px 0 30px",
-                                        fontSize: "44px",
-                                        fontWeight: "bold",
-                                        color: "white"
+                                        gap: "20px",
+                                        height: "152px",
+                                        boxShadow: "0 8px 30px 0 rgba(0,0,0,.15)",
+                                        borderRadius: "12px",
                                     }
                                 }
                             >
-                                <span>{item.number}</span>
+                                <div
+                                    style=
+                                    {
+                                        { 
+                                            backgroundColor: item.bgColor,
+                                            display: "flex",
+                                            justifyContent: "center",
+                                            alignItems: "center",
+                                            borderRadius: "21px 120px 120px 21px",
+                                            padding: "0 40px 0 30px",
+                                            fontSize: "44px",
+                                            fontWeight: "bold",
+                                            color: "white"
+                                        }
+                                    }
+                                >
+                                    <span>{item.number}</span>
+                                </div>
+                                <div style={{display: "flex", flexDirection: "column", justifyContent: "space-evenly"}}> 
+                                    <h4>{item.title}</h4>
+                                    <p style={{fontSize: "12px", lineHeight: "1.2", color: "#858585"}}>{item.description}</p>
+                                </div>
                             </div>
-                            <div style={{display: "flex", flexDirection: "column", justifyContent: "space-evenly"}}> 
-                                <h4>{item.title}</h4>
-                                <p style={{fontSize: "12px", lineHeight: "1.2", color: "#858585"}}>{item.description}</p>
-                            </div>
-                        </div>
                     </SwiperSlide>
                 ))}
             </Swiper>
---
+++ src/Components/Suggestions/suggestion.jsx
@@ -0,0 +1,40 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import 'swiper/swiper-bundle.css';
+import { suggestionsAm } from "../../../dataAm";
+import { Navigation } from "swiper/modules";
+
+export default function Suggestions() {
+    return (
+        <div className="px-3 product340x:px-7 product660x:px-28">
+            <div>
+                <h1 className="text-3xl font-bold">{suggestionsAm.title}</h1>
+            </div>
+            <Swiper
+                slidesPerView="auto"
+                spaceBetween={-70}
+                pagination={{ clickable: false }}
+                modules={[Navigation]}
+            >
+                {suggestionsAm.products.map((item) => (
+                    <SwiperSlide key={item.id}>
+                        <div
+                            className="relative flex justify-center bg-cover bg-center items-end h-[532px] py-8 px-7 text-white rounded-[20px] hover:-translate-y-5 transition-all duration-500 cursor-pointer w-[348px]"
+                            style={{ backgroundImage: `url(${item.imageUrl})` }}
+                        >
+                            <div className="absolute inset-0 bg-black opacity-40 rounded-[20px]" />
+                            <div className="relative flex flex-col gap-5 z-10">
+                                <p className="text-2xl font-bold">{item.title}</p>
+                                <p className="text-base">{item.description}</p>
+                                <button 
+                                    className={`w-auto md:w-[292px] py-3 text-white text-center transition-all duration-300 ${item.id == 1 ? "bg-[#E5326F]" : "bg-[#53079D]"} cursor-pointer text-sm font-semibold rounded-xl hover:brightness-[140%]`}
+                                >
+                                    Իմացի՛ր ավելին
+                                </button>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </div>
+    );
+}
