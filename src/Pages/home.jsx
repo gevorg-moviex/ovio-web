7c67221d9a9456867c8f7ac5fb977ad59626362e
Gevorg <gevorg.sahakyan.ait@gmail.com>
2024-11-05 13:50:27 +0400

    Some

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -23,7 +23,6 @@ export default function Home() {
             <News />
             <BottomContact />
             <DarkMode />
-            <PhoneService />
         </MainLayout>
         </>
     )
